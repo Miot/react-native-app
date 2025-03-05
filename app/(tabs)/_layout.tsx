b3a61f4a2c0f815a72fccee568e691581a722a8f
Miot <1405726849@qq.com>
2025-03-05 22:04:55 +0800

    learn router

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,6 +15,7 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarInactiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
@@ -34,10 +35,10 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="contact"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Contact Us',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="people.fill" color={color} />,
         }}
       />
     </Tabs>
---
+++ app/(tabs)/contact.jsx
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native";
+
+export default function Contact() {
+  return <View>
+    <Text>Contact Us</Text>
+  </View>
+}
